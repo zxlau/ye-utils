0f763de42c68afb10cfeb68d83e7fe4f51199e33
zxlau <657413201@qq.com>
2019-08-31 20:45:54 +0800

    feat: update ye-utils

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const utils = {
   },
   isArray: function (input) {
     return input instanceof Array || Object.prototype.toString.call(input) === '[object Array]';
+  },
+  isDate: function (input) {
+    return input instanceof Date || Object.prototype.toString.call(input) === '[object Date]';
+  },
+  isNumber: function (input) {
+    return input instanceof Number || Object.prototype.toString.call(input) === '[object Number]';
+  },
+  isString: function (input) {
+    return input instanceof String || Object.prototype.toString.call(input) === '[object String]';
+  },
+  isBoolean: function (input) {
+    return typeof input === 'boolean';
+  },
+  isFunction: function (input) {
+    return typeof input === 'function';
+  },
+  isNull: function (input) {
+    return input === undefined || input === null;
   }
 }
 export default utils;
